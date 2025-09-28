d140517830f8531b80c293aea754b9bc0c606842
Tolybay.Kuanov <tolybay.zh@gmail.com>
2025-09-28 06:58:35 +0500

    upd && add

--- app-admin/src/main/java/kz/app/appadmin/service/PermissionDao.java
+++ app-admin/src/main/java/kz/app/appadmin/service/PermissionDao.java
@@ -1,7 +1,15 @@
 package kz.app.appadmin.service;
 
+import kz.app.appcore.model.DbRec;
+import kz.app.appcore.utils.UtCnv;
+import kz.app.appcore.utils.UtDb;
+import kz.app.appcore.utils.XError;
 import kz.app.appdbtools.repository.Db;
 import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 
 @Component
@@ -13,8 +21,61 @@ public class PermissionDao {
         this.dbAdmin = dbAdmin;
     }
 
+    public List<DbRec> loadPermissions() throws Exception {
+        return dbAdmin.loadSql("""
+            select * from AuthUserGr where 0=0 order by ord
+        """, null);
+    }
 
+    private void validateRec(String id) throws Exception {
+        List<DbRec> stTmp = dbAdmin.loadSql("""
+                select r.name
+                from AuthRolePermis p
+                	left join authrole r on p.authrole=r.id
+                where p.permis=:id
+        """, Map.of("id", id));
+        if (!stTmp.isEmpty()) {
+            throw new XError("Используется в роли [{0}]", stTmp.getFirst().getString("name"));
+        }
 
+        stTmp = dbAdmin.loadSql("""
+            select r.fullname
+            from AuthUserPermis p
+            left join authuser r on p.authuser=r.id
+            where p.permis =:id
+        """, Map.of("id", id));
+        if (!stTmp.isEmpty()) {
+            throw new XError("Используется в привилегии пользователя [{0}]", stTmp.getFirst().getString("fullname"));
+        }
+    }
 
+    public void delete(DbRec rec) throws Exception {
+        validateRec(rec.getString("id"));
+
+        String sql = """
+            delete from Permis where id=:id;
+        """;
+        dbAdmin.execSql(sql, Map.of("id", rec.getString("id")));
+    }
+
+    public DbRec update(DbRec rec) throws Exception {
+        dbAdmin.updateRec("Permis", rec);
+        return dbAdmin.loadRec("Permis", rec.getLong("id"));
+    }
+
+    public DbRec insert(DbRec rec) throws Exception {
+        String sql = """
+            insert into Permis (id, parent, text, ord)
+            values (:id, :parent, :text, :ord)
+        """;
+
+        int ord = dbAdmin.loadSql("select max(ord) as max from Permis", null)
+                .getFirst().getInt("max");
+
+        rec.put("ord", ord+1);
+        dbAdmin.execSql(sql, rec);
+        //
+        return dbAdmin.loadRec("Permis", rec.getLong("id"));
+    }
 
 }
